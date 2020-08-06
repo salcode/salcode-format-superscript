89c3ea5e8a836cf7ef59fa9a614aa2ca23e24cc5
Sal Ferrarello <sal.ferrarello@webdevstudios.com>
2020-08-06 14:14:46 -0400

    Add root PHP file

---
+++ salcode-format-superscript.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: salcode Format Superscript
+ * Plugin URI: https://github.com/salcode/salcode-format-superscript
+ * Description: Add superscript to the WordPress block editor (a.k.a. Gutenberg).
+ * Version: 0.1.0
+ * Author: Sal Ferrarello
+ * Author URI: http://salferrarello.com/
+ * Text Domain: salcode-format-superscript
+ * Domain Path: /languages
+ *
+ * @package salcode-format-superscript
+ */
+
+namespace salcode\formatSuperscript;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
