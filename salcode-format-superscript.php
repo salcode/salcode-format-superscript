777d80a75494e9017c628118f1277c6d1b3f6f3c
Sal Ferrarello <sal.ferrarello@webdevstudios.com>
2020-08-06 14:30:51 -0400

    Enqueue JavaScript file

--- salcode-format-superscript.php
+++ salcode-format-superscript.php
@@ -18,3 +18,26 @@ namespace salcode\formatSuperscript;
 if ( ! defined( 'WPINC' ) ) {
 	die;
 }
+
+add_action(
+	'enqueue_block_editor_assets',
+	__NAMESPACE__ . '\salcode_format_superscript'
+);
+
+/**
+ * Register and enqueue JavaScript code.
+ *
+ * @return void
+ */
+function salcode_format_superscript() {
+	$plugin_data = get_plugin_data( __FILE__ );
+
+	wp_register_script(
+		'salcode-format-superscript',
+		plugins_url( 'js/salcode-format-superscript.js', __FILE__ ),
+		[ 'wp-rich-text' ],
+		$plugin_data['Version']
+	);
+
+	wp_enqueue_script( 'salcode-format-superscript' );
+}
